6fe470bd46bd56304362eb1e061bc05ea737886e
cnhguy <cnheric@gmail.com>
2019-02-17 15:57:16 -0800

    updated tool window interface

--- src/main/java/FootPrintToolWindowFactory.java
+++ src/main/java/FootPrintToolWindowFactory.java
@@ -1,26 +1,26 @@
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class FootPrintToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
-    }
-
-    @Override
-    public void init(ToolWindow window) {
-
+        FootPrintToolWindow fToolWindow = new FootPrintToolWindow(toolWindow);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(fToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
     }
 
     @Override
     public boolean shouldBeAvailable(@NotNull Project project) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isDoNotActivateOnStart() {
-        return false;
+        return true;
     }
 }
