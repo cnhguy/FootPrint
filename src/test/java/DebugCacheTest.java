553001643f357cc756e9d797de01bd24da5c35b4
Audrey Tran-Thien <atranthien@gmail.com>
2019-02-17 16:27:59 -0800

    added testing classes for cache and var info

---
+++ src/test/java/DebugCacheTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class DebugCacheTest {
+
+    DebugCache cache = new DebugCache();
+
+    @Test
+    public void testGet() {
+
+    }
+
+    @Test
+    public void testPut() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+
+
+}
---
+++ src/test/java/VariableInfoTest.java
@@ -0,0 +1,25 @@
+import com.sun.tools.jdi.BooleanValueImpl;
+import org.junit.Test;
+import com.sun.jdi.Value;
+
+public class VariableInfoTest {
+
+    @Test
+    public void testConstructor() {
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+
+    @Test
+    public void testValueAsString() {
+
+    }
+
+    @Test
+    public void testEquals() {
+
+    }
+}
